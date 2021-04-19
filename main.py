193913c848fd4272365572ffd23a3ce73fdf6cd4
Jean Catanho <jensmcatanho@gmail.com>
2021-04-18 21:39:12 -0300

    Generate report and sync to Dropbox

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
+from datetime import datetime
 import dropbox
 from dropbox.exceptions import ApiError, AuthError
 import speedtest
 import sys
 
 TOKEN=""
+FILENAME = "report.txt"
+
+def file_exists_remotelly(dbx, filename) -> bool:
+    path = "/{}".format(filename)
+    try:
+        dbx.files_get_metadata(path)
+        return True
+    except:
+        return False
+
+
+def download_file(dbx, filename):
+    try:
+        metadata, res = dbx.files_download("/{}".format(filename))        
+        with open("{}".format(filename), "wb") as f:
+            f.write(res.content)
+        
+    except ApiError as e:
+        sys.exit("Error: {}".format(e))
+
+
+def upload_file(dbx, filename):
+    path = "/{}".format(filename)
+    try:
+        with open("{}".format(filename), "rb") as f:
+            res = dbx.files_upload(f.read(), path, mode=dropbox.files.WriteMode.overwrite)
+
+    except ApiError as e:
+        sys.exit("Error: {}".format(e))
+
 
 if __name__ == '__main__':
     if len(TOKEN) == 0:
@@ -15,18 +46,20 @@ if __name__ == '__main__':
         except AuthError:
             sys.exit("Error: Invalid access token.")
 
-        try:
-            metadata, res = dbx.files_download("/Hello_World.txt")        
-            with open("Hello_World.txt", "wb") as f:
-                f.write(res.content)
+        st = speedtest.Speedtest()
+        st.download(threads=None)
+        st.upload(threads=None)
+        results = st.results
+        result_to_sync = "{}\nDownload: {} Mbps\nUpload: {} Mbps\n\n".format(results.timestamp, results.download*(10**-6), results.upload*(10**-6))
+
+        if file_exists_remotelly(dbx, FILENAME):
+            download_file(dbx, FILENAME)
             
-        except ApiError as e:
-            sys.exit("Error: {}".format(e))
+            with open("{}".format(FILENAME), "a") as f:
+                f.write(result_to_sync)
 
-#    st = speedtest.Speedtest()
-#    st.download(threads=None)
-#    st.upload(threads=None)
-#    results = st.results
+        else:
+            with open("{}".format(FILENAME), "w") as f:
+                f.write(result_to_sync)
 
-#    print("Download: {} Mbps\nUpload: {} Mbps".format(results.download*(10**-6), results.upload*(10**-6)))
-
+        upload_file(dbx, FILENAME)
