055e32b354c882ff13969d41f77b6f919a582282
Jean Catanho <jensmcatanho@gmail.com>
2021-04-19 22:59:38 -0300

    Add f-strings, pathlib and type hinting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import dropbox
 from dropbox.exceptions import ApiError, AuthError
 import os
 import pandas
+from pathlib import Path
 import pytz
 import speedtest
 import sys
@@ -10,36 +11,42 @@ import sys
 TOKEN=""
 FILENAME = "report.xlsx"
 
-def file_exists_remotelly(dbx, filename) -> bool:
-    path = "/{}".format(filename)
+def file_exists_remotelly(dbx: dropbox.Dropbox, filename: str) -> bool:
     try:
-        dbx.files_get_metadata(path)
+        remote_report_file_path = f"/{filename}"
+        dbx.files_get_metadata(remote_report_file_path)
         return True
+
     except:
         return False
 
 
-def download_file(dbx, filename):
+def download_file(dbx: dropbox.Dropbox, filename: str):
     try:
-        metadata, res = dbx.files_download("/{}".format(filename))        
-        with open("{}".format(filename), "wb") as f:
-            f.write(res.content)
+        local_report_file_path = Path(filename)
+        remote_report_file_path = f"/{filename}"
+
+        metadata, response = dbx.files_download(remote_report_file_path)
+        with local_report_file_path.open(mode='wb') as report_file:
+            report_file.write(response.content)
         
     except ApiError as e:
-        sys.exit("Error: {}".format(e))
+        sys.exit(f"Error: {e}")
 
 
-def upload_file(dbx, filename):
-    path = "/{}".format(filename)
+def upload_file(dbx: dropbox.Dropbox, filename: str):
     try:
-        with open("{}".format(filename), "rb") as f:
-            res = dbx.files_upload(f.read(), path, mode=dropbox.files.WriteMode.overwrite)
+        local_report_file_path = Path(filename)
+        remote_report_file_path = f"/{filename}"
+
+        with local_report_file_path.open(mode='rb') as report_file:
+            upload_result = dbx.files_upload(report_file.read(), remote_report_file_path, mode=dropbox.files.WriteMode.overwrite)
 
     except ApiError as e:
-        sys.exit("Error: {}".format(e))
+        sys.exit(f"Error: {e}")
 
 
-def write_results_to_xlsx(results):
+def write_results_to_xlsx(results: speedtest.SpeedtestResults):
     now = current_time()
     timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
     sheet_name = now.strftime("%Y-%m")
@@ -57,11 +64,11 @@ def write_results_to_xlsx(results):
         data_frame.to_excel(writer, sheet_name=sheet_name, index=False, float_format="%.2f")
 
 
-def bits_per_second_to_megabits_per_second(bits_per_second):
+def bits_per_second_to_megabits_per_second(bits_per_second: float) -> float:
     return bits_per_second * 0.000001
 
 
-def current_time():
+def current_time() -> datetime:
     tz = pytz.timezone('Brazil/East')
     return datetime.now(tz)
 
